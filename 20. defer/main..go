f5376f1890cc4a1f505e79d964b31b9a1128860d
Ckam <ckam225@gmail.com>
2022-06-30 13:51:33 +0000

    defer

---
+++ 20. defer/main..go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Functions
+func add(a1, a2 int) int {
+	res := a1 + a2
+	fmt.Println("Result: ", res)
+	return 0
+}
+
+// Main function
+func main() {
+
+	fmt.Println("Start")
+
+	// Multiple defer statements
+	// Executes in LIFO order
+	defer fmt.Println("End")
+	defer add(34, 56)
+	defer add(10, 10)
+
+	fmt.Println("END GAME")
+}
