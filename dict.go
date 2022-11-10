b797d2a4c5fc31bee7aeeac8a6d9adebb1921720
Ckam <ckam225@gmail.com>
2022-11-10 11:44:17 +0300

    code review map collection

--- dict.go
+++ dict.go
@@ -11,28 +11,53 @@ func Dict[K comparable, T any](m map[K]T) *dict[K, T] {
 }
 
 func (d *dict[K, T]) Split() (keys []K, values []T) {
-	for k, v := range d.items {
-		keys = append(keys, k)
-		values = append(values, v)
-	}
-	return keys, values
+	return SplitDict(d.items)
 }
 
-func (d *dict[K, T]) Keys(t map[K]T) (keys []K) {
-	for k := range t {
-		keys = append(keys, k)
-	}
-	return keys
+func (d *dict[K, T]) Keys() (keys []K) {
+	return DictKeys(d.items)
 }
 
 func (d *dict[K, T]) Values() (values []T) {
-	for _, v := range d.items {
-		values = append(values, v)
-	}
-	return values
+	return DictValues(d.items)
 }
 
-func SplitMap[K comparable, T interface{}](t map[K]T) (keys []K, values []T) {
+func (d *dict[K, T]) Has(key K) bool {
+	_, ok := d.items[key]
+	return ok
+}
+
+func (d *dict[K, T]) Get(key K) T {
+	return d.items[key]
+}
+
+func (d *dict[K, T]) Set(key K, value T) {
+	d.items[key] = value
+}
+
+func (d *dict[K, T]) Remove(key K) bool {
+	if d.Has(key) {
+		delete(d.items, key)
+		return true
+	}
+	return false
+}
+
+func (d *dict[K, T]) Map(f func(K, T) (K, T)) *dict[K, T] {
+	d.items = MapDict(d.items, f)
+	return d
+}
+
+func (d *dict[K, T]) Filter(f func(K, T) bool) *dict[K, T] {
+	d.items = FilterDict(d.items, f)
+	return d
+}
+
+func (d *dict[K, T]) Value() map[K]T {
+	return d.items
+}
+
+func SplitDict[K comparable, T interface{}](t map[K]T) (keys []K, values []T) {
 	for k, v := range t {
 		keys = append(keys, k)
 		values = append(values, v)
@@ -40,16 +65,35 @@ func SplitMap[K comparable, T interface{}](t map[K]T) (keys []K, values []T) {
 	return keys, values
 }
 
-func MapKeys[K comparable, T interface{}](t map[K]T) (keys []K) {
+func DictKeys[K comparable, T interface{}](t map[K]T) (keys []K) {
 	for k := range t {
 		keys = append(keys, k)
 	}
 	return keys
 }
 
-func MapValues[K comparable, T interface{}](t map[K]T) (values []T) {
+func DictValues[K comparable, T interface{}](t map[K]T) (values []T) {
 	for _, v := range t {
 		values = append(values, v)
 	}
 	return values
+}
+
+func FilterDict[K comparable, T interface{}](dt map[K]T, f func(K, T) bool) map[K]T {
+	m := make(map[K]T)
+	for k, v := range dt {
+		if f(k, v) {
+			m[k] = v
+		}
+	}
+	return m
+}
+
+func MapDict[K comparable, T interface{}](dt map[K]T, f func(K, T) (K, T)) map[K]T {
+	m := make(map[K]T)
+	for k, v := range dt {
+		key, value := f(k, v)
+		m[key] = value
+	}
+	return m
 }
